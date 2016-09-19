4730663b3db3332e35c01842cfe1dc01f34d35fc
betterelephant <betterelephant@gmail.com>
2016-09-19 08:26:54 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var bodyParser = require('body-parser');
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { ccosts: "0", revenue: "0" });
+});
+
+router.post('/ccosts', function(req, res) {
+  var ccosts = req.body.ccosts
+  res.render('index', { ccosts: (ccosts) });
+});
+
+router.post('/revenue', function(req, res) {
+  var revenue = req.body.revenue
+  res.render('index', { ccosts: (ccosts), revenue: (revenue) });
+});
+
+
+module.exports = router;
